7a72a1e33a01f6f27005351dcaa346be30441281
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-06-17 18:39:52 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
-
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formRef = document.querySelector('.form');
 formRef.addEventListener('submit', onSubmitForm);
 
+const options = {
+  width: '400px',
+  fontSize: '18px',
+  position: 'center-bottom',
+  distance: '33px',
+  borderRadius: '13px',
+  timeout: 8000,
+  clickToClose: true,
+  cssAnimationStyle: 'from-right',
+};
+
 function onSubmitForm(e) {
   e.preventDefault();
 
-  let delay = Number(e.currentTarget.delay.value);
-  const step = Number(e.currentTarget.step.value);
-  const amount = Number(e.currentTarget.amount.value);
+  let delay = Number(e.target.delay.value);
+  const step = Number(e.target.step.value);
+  const amount = Number(e.target.amount.value);
 
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         setTimeout(() => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-            useIcon: false,
-          });
+          Notify.success(
+            `Fulfilled promise ${position} in ${delay}ms`,
+            options
+          );
         }, delay);
       })
       .catch(({ position, delay }) => {
         setTimeout(() => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-            useIcon: false,
-          });
+          Notify.failure(`Rejected promise ${position} in ${delay}ms`, options);
         }, delay);
       });
     delay += step;
